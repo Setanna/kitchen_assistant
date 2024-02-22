8c886c2e7fbf18f302ef7bb3a68e23bc9a9455d1
Setanna <joenhendrikhoelckhansen@gmail.com>
2024-02-22 11:25:03 +0100

    Added: basic dark and light theme with button

---
+++ lib/theme.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class ThemeClass {
+  Color lightPrimaryColor = Colors.lightBlue;
+  Color darkPrimaryColor = Colors.indigo;
+
+  static ThemeData lightTheme = ThemeData(
+      useMaterial3: true,
+      colorScheme: const ColorScheme.dark().copyWith(
+        primary: _themeClass.lightPrimaryColor,
+        background: Colors.white,
+      ),
+      textButtonTheme: TextButtonThemeData(
+          style: ButtonStyle(
+        minimumSize: MaterialStateProperty.all(
+          const Size(double.infinity, 55),
+        ),
+        backgroundColor: MaterialStateProperty.resolveWith<Color>(
+            (states) => _themeClass.lightPrimaryColor),
+        foregroundColor:
+            MaterialStateProperty.resolveWith<Color>((states) => Colors.white),
+      )));
+
+  static ThemeData darkTheme = ThemeData(
+      useMaterial3: true,
+      colorScheme: const ColorScheme.dark().copyWith(
+        primary: _themeClass.darkPrimaryColor,
+        background: Colors.indigo.shade800,
+      ),
+      textButtonTheme: TextButtonThemeData(
+          style: ButtonStyle(
+        minimumSize: MaterialStateProperty.all(
+          const Size(double.infinity, 55),
+        ),
+        backgroundColor: MaterialStateProperty.resolveWith<Color>(
+            (states) => _themeClass.darkPrimaryColor),
+        foregroundColor:
+            MaterialStateProperty.resolveWith<Color>((states) => Colors.white),
+      )));
+}
+
+ThemeClass _themeClass = ThemeClass();
