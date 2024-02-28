e105a94a8a1d0915f558f9cfb5811c7dd4dafe37
Setanna <joenhendrikhoelckhansen@gmail.com>
2024-02-28 08:18:52 +0100

    Added: made ingredients list reverse to add new ones at the bottom

--- lib/serving_sizes.dart
+++ lib/serving_sizes.dart
@@ -1,8 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-class ServingSizes extends StatelessWidget {
+class ServingSizes extends StatefulWidget {
   const ServingSizes({super.key});
+
+  @override
+  ServingSizesState createState() => ServingSizesState();
+}
+
+class Ingredient {
+  final String name;
+  final String unit;
+  final int amount;
+
+  Ingredient(this.name, this.unit, this.amount);
+}
+
+class ServingSizesState extends State<ServingSizes> {
+  List<Ingredient> ingredients = [Ingredient("Minced Meat", "kg", 2)];
+  List<Ingredient> reversedIngredients = [Ingredient("Minced Meat", "kg", 2)];
 
   @override
   Widget build(BuildContext context) {
@@ -53,11 +69,79 @@ class ServingSizes extends StatelessWidget {
               ),
             ],
           ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: reversedIngredients.length,
+              key: UniqueKey(),
+              itemBuilder: (context, index) => _buildRow(index),
+            ),
+          ),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
-              ElevatedButton(onPressed: () {}, child: Text("Add Ingredient"))
+              ElevatedButton(
+                  onPressed: () {
+                    ingredients.insert(
+                        ingredients.length, Ingredient("test", "test", 2));
+                    reversedIngredients = ingredients.reversed.toList();
+                    setState(() {});
+                  },
+                  child: const Text("Add Ingredient"))
             ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  _buildRow(index) {
+    return Dismissible(
+      key: UniqueKey(),
+      onDismissed: (direction) {
+        // Remove the item from the data source.
+        setState(() {
+          ingredients.removeAt(index);
+          reversedIngredients = ingredients.reversed.toList();
+        });
+      },
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Expanded(
+            flex: 3,
+            child: TextField(
+              textAlign: TextAlign.center,
+              keyboardType: TextInputType.number,
+              inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+              decoration: InputDecoration(
+                labelText: ingredients[index].name,
+                floatingLabelAlignment: FloatingLabelAlignment.center,
+                alignLabelWithHint: true,
+                hintText: 'Original Ingredient',
+              ),
+            ),
+          ),
+          const Expanded(
+            flex: 2,
+            child: Icon(
+              Icons.arrow_forward_rounded,
+              size: 30.0,
+            ),
+          ),
+          Expanded(
+            flex: 3,
+            child: TextField(
+              enabled: false,
+              textAlign: TextAlign.center,
+              keyboardType: TextInputType.number,
+              inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+              decoration: const InputDecoration(
+                labelText: 'New Ingredient',
+                floatingLabelAlignment: FloatingLabelAlignment.center,
+                alignLabelWithHint: true,
+                hintText: 'New Ingredient',
+              ),
+            ),
           ),
         ],
       ),
